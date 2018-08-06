a486c66b575589be56095a3c0ba362ecb2d05183
foxzhong <foxzhong@tencent.com>
2018-08-06 19:20:13 +0800

    alpine_cmd_with_git_code

---
+++ cmd/alpine_cmd_with_git_code/builder.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+const baseSpace = "/root/src"
+
+// Builder is
+type Builder struct {
+	GitCloneURL string
+	GitRef      string
+
+	Cmd string
+
+	projectName string
+}
+
+// NewBuilder is
+func NewBuilder(envs map[string]string) (*Builder, error) {
+	b := &Builder{}
+
+	if envs["GIT_CLONE_URL"] != "" {
+		b.GitCloneURL = envs["GIT_CLONE_URL"]
+		b.GitRef = envs["GIT_REF"]
+	} else if envs["_WORKFLOW_GIT_CLONE_URL"] != "" {
+		b.GitCloneURL = envs["_WORKFLOW_GIT_CLONE_URL"]
+		b.GitRef = envs["_WORKFLOW_GIT_REF"]
+	} else {
+		return nil, fmt.Errorf("envionment variable GIT_CLONE_URL is required")
+	}
+
+	if b.GitRef == "" {
+		b.GitRef = "master"
+	}
+
+	s := strings.TrimSuffix(strings.TrimSuffix(b.GitCloneURL, "/"), ".git")
+	b.projectName = s[strings.LastIndex(s, "/")+1:]
+
+	if b.GitRef = envs["GIT_REF"]; b.GitRef == "" {
+		b.GitRef = "master"
+	}
+
+	b.Cmd = envs["CMD"]
+	return b, nil
+}
+
+func (b *Builder) run() error {
+	if err := b.gitPull(); err != nil {
+		return err
+	}
+
+	if err := b.gitReset(); err != nil {
+		return err
+	}
+
+	if err := b.execCmd(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *Builder) gitPull() error {
+	var command = []string{"git", "clone", "--recurse-submodules", b.GitCloneURL, b.projectName}
+	if _, err := (CMD{Command: command}).Run(); err != nil {
+		fmt.Println("Clone project failed:", err)
+		return err
+	}
+	fmt.Println("Clone project", b.GitCloneURL, "succeded.")
+	return nil
+}
+
+func (b *Builder) gitReset() error {
+	cwd, _ := os.Getwd()
+	var command = []string{"git", "reset", "--hard", b.GitRef}
+	if _, err := (CMD{command, filepath.Join(cwd, b.projectName)}).Run(); err != nil {
+		fmt.Println("Switch to commit", b.GitRef, "failed:", err)
+		return err
+	}
+	fmt.Println("Switch to", b.GitRef, "succeded.")
+	return nil
+}
+
+func (b *Builder) execCmd() error {
+	command := []string{"/bin/sh", "-c", b.Cmd}
+
+	_, err := (CMD{Command: command}).Run()
+	if err != nil {
+		fmt.Println("exec CMD failed:", err)
+		return err
+	}
+
+	return nil
+}
+
+type CMD struct {
+	Command []string // cmd with args
+	WorkDir string
+}
+
+func (c CMD) Run() (string, error) {
+	cmdStr := strings.Join(c.Command, " ")
+	fmt.Printf("[%s] Run CMD: %s\n", time.Now().Format("2006-01-02 15:04:05"), cmdStr)
+
+	cmd := exec.Command(c.Command[0], c.Command[1:]...)
+	if c.WorkDir != "" {
+		cmd.Dir = c.WorkDir
+	}
+
+	data, err := cmd.CombinedOutput()
+	result := string(data)
+	if len(result) > 0 {
+		fmt.Println(result)
+	}
+
+	return result, err
+}
