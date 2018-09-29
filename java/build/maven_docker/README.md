e4ee46d2410ab04b6e580b452855d3e4ff9aba27
houmingtao <houmingtao@kingsoft.com>
2018-09-29 19:11:23 +0800

    增加maven docker:build的支持

---
+++ java/build/maven_docker/README.md
@@ -0,0 +1,32 @@
+## 组件名称：Java Build Maven
+
+使用 maven 进行 java 项目构建, 可选地将构建产物上传到指定的二进制仓库.
+
+### 组件参数
+#### 入参
+- `GIT_CLONE_URL` 必填，源代码地址，如为私有仓库需要授权; 如需使用系统关联的git仓库, 可以从系统提供的全局环境变量中获取: `${_WORKFLOW_GIT_CLONE_URL}`
+- `GIT_REF` 非必填，源代码目标提交号或者分支名, 默认为master
+- `M2_SETTINGS_XML` 非必填，`$user/.m2/setting.xml`配置文件内容，默认使用maven的全局配置
+- `GOALS` 非必填，maven 构建目标, 默认是`package`
+- `POM_PATH` 非必填，pom 文件相对路径, 默认`./pom.xml`
+- `HUB_REPO` 非必填，构建产物目标二进制仓库, 组件在构建完成后将尝试自动上传构建产物到此仓库, 如果此值为空将不会自动上传
+- `HUB_USER` 非必填，上传构建产物对应仓库的用户名, 如果想使用当前主账号, 可以直接设置`_WORKFLOW_FLAG_HUB_TOKEN: true`, 执行引擎将自动注入当前用户名和token
+- `HUB_TOKEN` 非必填，上传构建产物对应仓库的用户密码或者token, 同上, 如果设置了`_WORKFLOW_FLAG_HUB_TOKEN: true`, 此入参可以省略
+- `ARTIFACT_PATH` 非必填，上传构建产物对应的仓库目录, 默认是仓库根目录
+- `ARTIFACT_TAG` 非必填，上传构建产物对应的tag, 默认是latest
+
+#### 出参
+- `ARTIFACT` 构建产物结果列表
+- `ARTIFACT_URL` 成功上传的构建产物url
+
+### Tag列表及其Dockerfile链接
+
+* 3.5-jdk-8, latest: [Dockerfile](https://github.com/tencentyun/workflow-components/blob/c2d0c1ceb447694a092599858203d29dd877e6bb/java/build/maven/Dockerfile)
+
+### 源码地址
+
+Java Maven Build: <https://github.com/tencentyun/workflow-components/tree/master/java/build/maven>
+
+### 构建:
+
+`docker build -t hub.tencentyun.com/tencenthub/java_build_maven:latest .`
---
+++ java/build/maven_docker/builder.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+const baseSpace = "/root/src"
+
+type Builder struct {
+	// 用户提供参数, 通过环境变量传入
+	GitCloneURL string
+	GitRef      string
+	Goals       string
+	PomPath     string
+
+	HubRepo      string
+	HubUser      string
+	HubToken     string
+	M2SettingXML string
+
+	projectName string
+}
+
+// NewBuilder is
+func NewBuilder(envs map[string]string) (*Builder, error) {
+	b := &Builder{}
+
+	if envs["GIT_CLONE_URL"] != "" {
+		b.GitCloneURL = envs["GIT_CLONE_URL"]
+		b.GitRef = envs["GIT_REF"]
+	} else if envs["_WORKFLOW_GIT_CLONE_URL"] != "" {
+		b.GitCloneURL = envs["_WORKFLOW_GIT_CLONE_URL"]
+		b.GitRef = envs["_WORKFLOW_GIT_REF"]
+	} else {
+		return nil, fmt.Errorf("envionment variable GIT_CLONE_URL is required")
+	}
+
+	if b.GitRef == "" {
+		b.GitRef = "master"
+	}
+
+	s := strings.TrimSuffix(strings.TrimSuffix(b.GitCloneURL, "/"), ".git")
+	b.projectName = s[strings.LastIndex(s, "/")+1:]
+
+	if b.GitRef = envs["GIT_REF"]; b.GitRef == "" {
+		b.GitRef = "master"
+	}
+
+	b.Goals = strings.TrimSpace(strings.TrimPrefix(strings.TrimSpace(envs["GOALS"]), "mvn "))
+
+	if b.PomPath = envs["POM_PATH"]; b.PomPath == "" {
+		b.PomPath = "./pom.xml"
+	}
+
+	b.HubUser = envs["HUB_USER"]
+	b.HubToken = envs["HUB_TOKEN"]
+
+	if b.HubUser == "" && b.HubToken == "" {
+		b.HubUser = envs["_WORKFLOW_HUB_USER"]
+		b.HubToken = envs["_WORKFLOW_HUB_TOKEN"]
+	}
+	if b.HubUser == "" || b.HubToken == "" {
+		return nil, fmt.Errorf("envionment variable HUB_USER, HUB_TOKEN are required")
+	}
+
+	b.HubRepo = envs["HUB_REPO"]
+
+	b.M2SettingXML = envs["M2_SETTINGS_XML"]
+
+	return b, nil
+}
+
+func (b *Builder) run() error {
+	if err := os.Chdir(baseSpace); err != nil {
+		return fmt.Errorf("Chdir to baseSpace(%s) failed:%v", baseSpace, err)
+	}
+
+	if err := b.gitPull(); err != nil {
+		return err
+	}
+
+	if err := b.gitReset(); err != nil {
+		return err
+	}
+
+	if err := b.setM2SettingXML(); err != nil {
+		return err
+	}
+
+	if err := b.build(); err != nil {
+		return err
+	}
+
+	// if err := b.handleArtifacts(); err != nil {
+	// 	return err
+	// }
+
+	// err = b.doPush(b.Image)
+	// if err != nil {
+	// 	return
+	// }
+	return nil
+}
+
+func (b *Builder) setM2SettingXML() error {
+	var command = []string{"cp", "/usr/share/maven/conf/settings.xml", "/root/.m2/settings.xml"}
+	if _, err := (CMD{Command: command}).Run(); err != nil {
+		fmt.Println("copy settings.xml failed:", err)
+		return err
+	}
+	if b.M2SettingXML != "" {
+		command = []string{"echo", b.M2SettingXML, ">", "/root/.m2/setting.xml"}
+		if _, err := (CMD{Command: command}).Run(); err != nil {
+			fmt.Println("write setting to .m2/settings.xml failed:", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func (b *Builder) build() error {
+	var command = strings.Fields(b.Goals)
+
+	if len(command) == 0 {
+		command = append(command, "mvn", "package")
+	}
+
+	if command[0] != "mvn" {
+		command = append([]string{"mvn"}, command...)
+	}
+
+	command = append(command, "-f", b.PomPath)
+
+	cwd, _ := os.Getwd()
+	if _, err := (CMD{command, filepath.Join(cwd, b.projectName)}).Run(); err != nil {
+		fmt.Println("Run mvn goals failed:", err)
+		return err
+	}
+	fmt.Println("Run mvn goals succeed.")
+	return nil
+}
+
+func (b *Builder) gitPull() error {
+	var command = []string{"git", "clone", "--recurse-submodules", b.GitCloneURL, b.projectName}
+	if _, err := (CMD{Command: command}).Run(); err != nil {
+		fmt.Println("Clone project failed:", err)
+		return err
+	}
+	fmt.Println("Clone project", b.GitCloneURL, "succeed.")
+	return nil
+}
+
+func (b *Builder) gitReset() error {
+	cwd, _ := os.Getwd()
+	var command = []string{"git", "checkout", b.GitRef, "--"}
+	if _, err := (CMD{command, filepath.Join(cwd, b.projectName)}).Run(); err != nil {
+		fmt.Println("Switch to commit", b.GitRef, "failed:", err)
+		return err
+	}
+	fmt.Println("Switch to", b.GitRef, "succeed.")
+	return nil
+}
+
+type CMD struct {
+	Command []string // cmd with args
+	WorkDir string
+}
+
+func (c CMD) Run() (string, error) {
+	fmt.Println("Run CMD: ", strings.Join(c.Command, " "))
+
+	cmd := exec.Command(c.Command[0], c.Command[1:]...)
+	if c.WorkDir != "" {
+		cmd.Dir = c.WorkDir
+	}
+
+	data, err := cmd.CombinedOutput()
+	result := string(data)
+	if len(result) > 0 {
+		fmt.Println(result)
+	}
+
+	return result, err
+}
